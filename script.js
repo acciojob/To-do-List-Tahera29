dcbd649396785a3cadcee94f7483fb51a7172572
Tahera29 <120545807+Tahera29@users.noreply.github.com>
2023-02-14 21:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
-
+var todoValue= document.getElementById("newTodoInput");
+var btn= document.getElementById("addTodoBtn");
+var ol= document.getElementById("todoList");
+function addTodo()
+	{
+	if(todoValue.value){
+	//an empty string should not be submitted
+	//we need to create list
+	var abc= document.createElement('li');
+	abc.textContent= todoValue.value;
+	ol.appenChild(li);
+	todoValue.value="";
+	
+	}
+}
+btn.addEventListener('click',addTodo)
